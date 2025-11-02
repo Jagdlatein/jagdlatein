c915b2f440077cfdfdf542a31adbc2cf7735e97d
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-02 19:42:22 +0100

    Update preise.js

--- pages/preise.js
+++ pages/preise.js
@@ -8,13 +8,22 @@ export default function Preise(){
           <div className="card">
             <h3>Monat</h3>
             <p style={{fontSize:24,margin:'8px 0'}}><b>10 €</b> / Monat</p>
-            <ul><li>Alle Kurse DE / AT / CH</li><li>Quiz & Karteikarten</li><li>Neue Inhalte inklusive</li></ul>
+            <ul>
+              <li>Alle Kurse DE / AT / CH</li>
+              <li>Quiz & Karteikarten</li>
+              <li>Neue Inhalte inklusive</li>
+            </ul>
             <a className="cta" href="/login" style={{display:'inline-block',marginTop:12}}>Weiter</a>
           </div>
+
           <div className="card">
             <h3>Jahr</h3>
             <p style={{fontSize:24,margin:'8px 0'}}><b>100 €</b> / Jahr</p>
-            <ul><li>Alle Kurse DE / AT / CH</li><li>Quiz & Karteikarten</li><li>2 Monate geschenkt</li></ul>
+            <ul>
+              <li>Alle Kurse DE / AT / CH</li>
+              <li>Quiz & Karteikarten</li>
+              <li>2 Monate geschenkt</li>
+            </ul>
             <a className="cta" href="/login" style={{display:'inline-block',marginTop:12}}>Weiter</a>
           </div>
         </div>
