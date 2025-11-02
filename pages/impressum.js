02477cd752c10d8f2284bdfea385f4581f2e1eae
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-02 19:45:56 +0100

    Update impressum.js

--- pages/impressum.js
+++ pages/impressum.js
@@ -1,32 +1,19 @@
-export default function Impressum() {
+export default function Impressum(){
   return (
-    <>
-      <header className="header">
-        <div className="container header-inner">
-          <div className="brand">
-            <div style={{ width: 28, height: 28, background: '#D4AF37', borderRadius: '50%' }} />
-            <div>Jagdlatein <span>• Lernplattform</span></div>
-          </div>
-          <nav className="menu">
-            <a href="/">Start</a>
-            <a href="/preise">Preise</a>
-          </nav>
-        </div>
-      </header>
+    <section className="section">
+      <div className="container">
+        <h1>Impressum</h1>
 
-      <section className="section">
-        <div className="container">
-          <h1>Impressum</h1>
-          <p><b>Verantwortlich:</b><br/>jagdlatein</p>
-          <p>
-            Kirchweg 3<br/>
-            7317 Valens<br/>
-            St. Gallen, Schweiz
-          </p>
-          <p><b>E-Mail:</b> info@jagdlatein.de</p>
-          <p className="small">Angaben ohne Gewähr. Länderspezifische Rechtsvorgaben (DE/CH/AT) beachten.</p>
-        </div>
-      </section>
-    </>
+        <p><b>Dienstanbieter:</b><br/>Jagdlatein</p>
+
+        <p><b>Kontakt:</b><br/>
+        E-Mail: <a href="mailto:info@jagdlatein.de">info@jagdlatein.de</a></p>
+
+        <p className="small">
+          Hinweis: Dies ist ein Angebot aus der Schweiz. Angaben ohne Gewähr. 
+          Länderspezifische Rechtsvorschriften (DE/AT/CH) sind vom Nutzer zu beachten.
+        </p>
+      </div>
+    </section>
   )
 }
