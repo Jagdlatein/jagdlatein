0a352b9c75e42732b169a632aa7b654e58745981
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-02 08:17:20 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,3 +39,6 @@ export default function Home() {
     </>
   )
 }
+export default function Home() {
+  return <div style={{padding:20, fontFamily:'system-ui'}}>Hallo Jagdlatein 👋 – Build-Test OK</div>;
+}
