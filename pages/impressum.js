6f729d770217ddf2c66f8fb46f9d53cdc3215260
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-03 18:10:13 +0100

    Update impressum.js

--- pages/impressum.js
+++ pages/impressum.js
@@ -1,19 +1,17 @@
+import Seo from '../components/Seo';
+
 export default function Impressum(){
   return (
-    <section className="section">
-      <div className="container">
-        <h1>Impressum</h1>
-
-        <p><b>Dienstanbieter:</b><br/>Jagdlatein</p>
-
-        <p><b>Kontakt:</b><br/>
-        E-Mail: <a href="mailto:info@jagdlatein.de">info@jagdlatein.de</a></p>
-
-        <p className="small">
-          Hinweis: Dies ist ein Angebot aus der Schweiz. Angaben ohne Gewähr. 
-          Länderspezifische Rechtsvorschriften (DE/AT/CH) sind vom Nutzer zu beachten.
-        </p>
-      </div>
-    </section>
-  )
+    <>
+      <Seo title="Impressum – Jagdlatein" description="Kontaktinformationen und rechtliche Hinweise." />
+      <section className="section">
+        <div className="container">
+          <h1>Impressum</h1>
+          <p><b>Dienstanbieter:</b><br/>Jagdlatein</p>
+          <p><b>Kontakt:</b><br/>E-Mail: <a href="mailto:info@jagdlatein.de">info@jagdlatein.de</a></p>
+          <p className="small">Hinweis: Angebot aus der Schweiz. Länderspezifische Rechtsvorgaben (DE/AT/CH) bitte beachten.</p>
+        </div>
+      </section>
+    </>
+  );
 }
