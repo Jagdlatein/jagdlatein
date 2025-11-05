1468acc246a2d8f5a0f9127a8f0b00fd91fb5e1b
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-05 20:41:56 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 // pages/_app.js
-import "../styles/globals.css";
+import "../styles/globals.css"; // falls vorhanden, ansonsten weglassen
 import Layout from "../components/Layout";
-import CookieBanner from "../components/CookieBanner";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <CookieBanner />
     </Layout>
   );
 }
