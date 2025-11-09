ebc0575e0c7d0391abb3b7698bf020f3ddb9dffb
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-09 12:27:26 +0100

    Update request-code.js

--- pages/api/admin/auth/request-code.js
+++ pages/api/admin/auth/request-code.js
@@ -1,5 +1,5 @@
 import prisma from "../../../lib/prisma";
-import { sendLoginCode } from "../../../lib/mail";
+import { sendLoginCode } from "../../../lib/email";
 
 export default async function handler(req, res) {
   try {
