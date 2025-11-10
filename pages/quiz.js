6df700940f14bbd2ec0d463df626302ba3a9a319
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-10 21:07:21 +0100

    Update quiz.js

--- pages/quiz.js
+++ pages/quiz.js
@@ -6,7 +6,7 @@ export default function QuizPage() {
       <Header />
       <main className="p-4 max-w-3xl mx-auto">
         <h2 className="text-2xl font-bold mb-4">Quiz</h2>
-        {/* dein Quiz-Inhalt hier */}
+        {/* ... dein Inhalt ... */}
       </main>
     </>
   );
