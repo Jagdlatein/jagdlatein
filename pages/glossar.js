3f34f2349e1fd2b02467a431c4f7506233fe7781
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-10 21:03:53 +0100

    Create glossar.js

---
+++ pages/glossar.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+export default function GlossarPage() {
+  return (
+    <>
+      <Header />
+      <main className="p-4 max-w-3xl mx-auto">
+        <h2 className="text-2xl font-bold mb-4">Glossar</h2>
+        {/* Glossar-Inhalt */}
+      </main>
+    </>
+  );
+}
