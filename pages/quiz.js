e070dd853ebb405f29569805176348166e265cbe
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-10 21:01:29 +0100

    Create quiz.js

---
+++ pages/quiz.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+export default function QuizPage() {
+  return (
+    <>
+      <Header />
+      <main className="p-4 max-w-3xl mx-auto">
+        <h2 className="text-2xl font-bold mb-4">Quiz</h2>
+        {/* dein Quiz-Inhalt hier */}
+      </main>
+    </>
+  );
+}
