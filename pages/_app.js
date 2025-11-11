8453d046c4f27ee717b6643c811664ccc9c612bf
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-11 10:58:19 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,7 @@ import "../styles/globals.css";
 import Header from "../components/Header";
 
 export default function App({ Component, pageProps, router }) {
-  // Header nur anzeigen, wenn der User eingeloggt ist (nicht auf /login oder /preise)
   const hideHeader = router.pathname === "/login" || router.pathname === "/preise";
-
   return (
     <>
       {!hideHeader && <Header />}
