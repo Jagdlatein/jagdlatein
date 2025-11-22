8f4040ba8d19cc3d6031eba246b975dd1481d492
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-22 22:11:15 +0100

    Update page.js

--- app/debug-cookies/page.js
+++ app/debug-cookies/page.js
@@ -1,9 +1,20 @@
-export default function DebugCookies() {
+"use client";
+
+export const dynamic = "force-dynamic";
+
+import { useEffect, useState } from "react";
+
+export default function DebugCookiesPage() {
+  const [clientCookies, setClientCookies] = useState("");
+
+  useEffect(() => {
+    setClientCookies(document.cookie);
+  }, []);
+
   return (
     <div style={{ padding: 20 }}>
       <h1>Cookie Debug</h1>
-      <p><strong>Cookies:</strong></p>
-      <pre>{document.cookie}</pre>
+      <pre>{clientCookies || "Keine Cookies gefunden"}</pre>
     </div>
   );
 }
