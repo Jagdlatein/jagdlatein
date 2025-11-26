4ef5d646c263784107c3bf47d72031cb5f84003d
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-26 18:26:09 +0100

    Update page.jsx

--- app/quiz/run/page.jsx
+++ app/quiz/run/page.jsx
@@ -1,11 +1,18 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-const QuizClient = dynamic(() => import("./QuizClient"), {
-  ssr: false,
-});
+const QuizClient = dynamic(() => import("./QuizClient"), { ssr: false });
 
 export default function QuizRunPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const u = localStorage.getItem("jagd_username");
+    if (!u) router.replace("/quiz/username");
+  }, []);
+
   return <QuizClient />;
 }
