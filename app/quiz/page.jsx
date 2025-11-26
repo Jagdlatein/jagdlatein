d01a59777e3dee4366e54d6199f7f9631a57e498
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-26 05:34:30 +0100

    Update page.jsx

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function QuizIndexPage() {
-  redirect("/quiz/run");
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/quiz/run");
+  }, []);
+
+  return null;
 }
