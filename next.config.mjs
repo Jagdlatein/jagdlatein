fb40885b2b1b9eb0c9bfe3d4aa779e29a6252291
Jagdlatein <hannes.ziegler@hotmail.com>
2025-11-28 16:30:50 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,34 @@
-export default { reactStrictMode: true }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/quiz",
+        destination: "/quiz-app",
+        permanent: true,
+      },
+      {
+        source: "/quiz/run",
+        destination: "/quiz-app/run",
+        permanent: true,
+      },
+      {
+        source: "/quiz/leaderboard",
+        destination: "/quiz-app/leaderboard",
+        permanent: true,
+      },
+      {
+        source: "/quiz/stats",
+        destination: "/quiz-app/stats",
+        permanent: true,
+      },
+      {
+        source: "/quiz/username",
+        destination: "/quiz-app/username",
+        permanent: true,
+      }
+    ];
+  },
+};
+
+module.exports = nextConfig;
