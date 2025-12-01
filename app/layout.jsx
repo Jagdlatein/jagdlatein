c583eb4f9690425bc6079746c0fa07e511383b57
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-01 11:49:35 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,15 @@
-import "./globals.css";   // 💥 WICHTIG!!!
+import "./globals.css";
 
 export const metadata = {
   title: "Jagdlatein",
   description: "Jagdquiz und Lernplattform für Jägerinnen und Jäger.",
+  icons: {
+    icon: [
+      { url: "/favicon_32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon_48.png", sizes: "48x48", type: "image/png" },
+    ],
+    apple: "/apple_touch_icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
