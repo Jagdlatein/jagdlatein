c76ac924bacb7ac173d819a4d51b62cd2cc411f0
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-01 17:30:25 +0100

    Delete app/api/jagdbuch directory

--- app/api/jagdbuch/posts/[slug]/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getPostBySlug } from "@/lib/jagdbuch";
-
-export async function GET(req, { params }) {
-  const post = await getPostBySlug(params.slug);
-  return NextResponse.json(post);
-}
