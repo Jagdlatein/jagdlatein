df7b29e727ac4f7c65c9781733561f7a8b416816
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-01 13:27:22 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,6 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
+      <head /> {/* WICHTIG! Ohne das kein Favicon! */}
       <body>{children}</body>
     </html>
   );
