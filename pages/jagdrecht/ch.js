7c6bfc971c2bbe85117373cbc6eb7ffd805887c3
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-02 06:59:08 +0100

    Create ch.js

---
+++ pages/jagdrecht/ch.js
@@ -0,0 +1,15 @@
+export default function JagdrechtCH() {
+  return (
+    <main style={{ maxWidth: 860, margin: "0 auto", padding: 32 }}>
+      <h1>🇨🇭 Jagdrecht Schweiz</h1>
+      <p>
+        Inhalte:  
+        – Kantonale Jagdgesetze  
+        – Bundesrecht CH  
+        – Patent-/Revierjagd  
+        – Schonzeiten / Schutzgebiete  
+        – Wildfolge & Schussabgabe  
+      </p>
+    </main>
+  );
+}
