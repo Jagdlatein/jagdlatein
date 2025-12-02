7ab17101d0ee914fc81db75e5ec4133864dc73c2
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-02 06:58:51 +0100

    Create at.js

---
+++ pages/jagdrecht/at.js
@@ -0,0 +1,14 @@
+export default function JagdrechtAT() {
+  return (
+    <main style={{ maxWidth: 860, margin: "0 auto", padding: 32 }}>
+      <h1>🇦🇹 Jagdrecht Österreich</h1>
+      <p>
+        Inhalte:  
+        – Landesjagdgesetze (tirol/bgl/...)  
+        – Waffenrecht AT  
+        – Schonzeiten & Bewirtschaftung  
+        – Abschusspläne  
+      </p>
+    </main>
+  );
+}
