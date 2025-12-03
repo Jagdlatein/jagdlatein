c132737ed6b51fb3178fe715a73098d9a3472ec2
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-03 17:14:49 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+    ],
+  },
+
   async redirects() {
     return [
       {
