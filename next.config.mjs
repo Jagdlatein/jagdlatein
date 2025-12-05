15ebc9ab32c464530c3f58690a74b7d31793dea7
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-05 16:19:42 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: false,   // 🔥 WICHTIG: App Router deaktivieren
+  },
+
   images: {
     remotePatterns: [
       {
