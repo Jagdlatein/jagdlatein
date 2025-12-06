17731cc808904380f68545e3fac2f4f67ba2b75c
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-06 10:19:42 +0100

    Create route.js

---
+++ app/api/jagdbuch/list/route.js
@@ -0,0 +1,14 @@
+import { supabase } from "../../../../lib/supabase";
+
+export async function GET() {
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*")
+    .order("id", { ascending: false });
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+  }
+
+  return new Response(JSON.stringify(data), { status: 200 });
+}
