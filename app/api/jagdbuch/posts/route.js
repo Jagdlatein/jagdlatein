64ef12b8d7ed6c6ce4fadcc318d2e47494c9b548
Jagdlatein <hannes.ziegler@hotmail.com>
2025-12-07 13:41:59 +0100

    Update route.js

--- app/api/jagdbuch/posts/route.js
+++ app/api/jagdbuch/posts/route.js
@@ -1,6 +1,11 @@
-import { supabase } from "@/lib/supabaseClient";
+import { createClient } from "@supabase/supabase-js";
 
 export const dynamic = "force-dynamic";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
 export async function GET() {
   const { data, error } = await supabase
