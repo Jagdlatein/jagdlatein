9f91b3aa8367642672f0d29336d1449dfac842e7
Jagdlatein <hannes.ziegler@hotmail.com>
2026-09-02 19:20:27 +0200

    Update krankeswild.js

--- pages/jagdpraxis/krankeswild.js
+++ pages/jagdpraxis/krankeswild.js
@@ -85,13 +85,12 @@ export default function KrankesWildErkennen() {
 
     if (isCorrect) setScore(score + 1);
 
-    setTimeout(() => {
-      setFeedback(null);
-      setLockButtons(false);
-      setStep(step + 1);
-    }, 1200);
-  }
-
+ setTimeout(() => {
+  setFeedback(null);
+  setLockButtons(false);
+  setStep((prev) => prev + 1);
+}, isCorrect ? 3500 : 1200);
+}
   // ------------------------------------------------------------
   // ENDANZEIGE
   // ------------------------------------------------------------
@@ -142,7 +141,10 @@ export default function KrankesWildErkennen() {
       <HomeButton />
       <h1 style={{ fontSize: 34, marginBottom: 10 }}>Krankes Wild erkennen</h1>
 
-      <ScenarioCard title={current.title} text={current.text} />
+     <ScenarioCard
+  title={current.title}
+  text={feedback === true ? current.text : null}
+/>
 
       <div
         style={{
