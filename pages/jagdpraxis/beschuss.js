b026f3d2a6c7c5018b3e878ca80cf35c33190e77
Jagdlatein <hannes.ziegler@hotmail.com>
2026-09-02 19:15:17 +0200

    Update beschuss.js

--- pages/jagdpraxis/beschuss.js
+++ pages/jagdpraxis/beschuss.js
@@ -85,12 +85,12 @@ export default function KugelfangSicherheit() {
     setFeedback(isCorrect);
     if (isCorrect) setScore(score + 1);
 
-    setTimeout(() => {
-      setFeedback(null);
-      setLockButtons(false);
-      setStep(step + 1);
-    }, 1200);
-  }
+  setTimeout(() => {
+  setFeedback(null);
+  setLockButtons(false);
+  setStep((prev) => prev + 1);
+}, isCorrect ? 3500 : 1200);
+}
 
   // ------------------------------------------------------------
   // ENDSEITE
@@ -147,7 +147,10 @@ export default function KugelfangSicherheit() {
         Kugelfang & Sicherheitstrainer
       </h1>
 
-      <ScenarioCard title={current.title} text={current.text} />
+    <ScenarioCard
+  title={current.title}
+  text={feedback === true ? current.text : null}
+/>
 
       <div
         style={{
