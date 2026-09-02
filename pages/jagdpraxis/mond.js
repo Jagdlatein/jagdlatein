3e2535164f22bf80fbcc567e7efee3215398099a
Jagdlatein <hannes.ziegler@hotmail.com>
2026-09-02 19:22:36 +0200

    Update mond.js

--- pages/jagdpraxis/mond.js
+++ pages/jagdpraxis/mond.js
@@ -84,12 +84,12 @@ export default function Mondphasen() {
     setFeedback(isCorrect);
     if (isCorrect) setScore(score + 1);
 
-    setTimeout(() => {
-      setFeedback(null);
-      setLockButtons(false);
-      setStep(step + 1);
-    }, 1200);
-  }
+   setTimeout(() => {
+  setFeedback(null);
+  setLockButtons(false);
+  setStep((prev) => prev + 1);
+}, isCorrect ? 3500 : 1200);
+}
 
   // ------------------------------------------------------------
   // ENDANZEIGE
@@ -146,7 +146,10 @@ export default function Mondphasen() {
         Mondphasen & Revieraktivität
       </h1>
 
-      <ScenarioCard title={current.title} text={current.text} />
+    <ScenarioCard
+  title={current.title}
+  text={feedback === true ? current.text : null}
+/>
 
       <div
         style={{
