85938635804b680a4755e7f412e576896f36cd43
Jagdlatein <hannes.ziegler@hotmail.com>
2026-09-02 18:04:24 +0200

    Update ansprache_rehwild.js

--- pages/jagdpraxis/ansprache_rehwild.js
+++ pages/jagdpraxis/ansprache_rehwild.js
@@ -85,10 +85,11 @@ export default function AnspracheRehwild() {
     if (isCorrect) setScore(score + 1);
 
     setTimeout(() => {
-      setFeedback(null);
-      setLockButtons(false);
-      setStep(step + 1);
-    }, 1200);
+  setFeedback(null);
+  setLockButtons(false);
+  setStep((prev) => prev + 1);
+}, isCorrect ? 3500 : 1200);
+}
   }
 
   // ------------------------------------------------------------
@@ -146,7 +147,10 @@ export default function AnspracheRehwild() {
         Rehwild sicher ansprechen
       </h1>
 
-      <ScenarioCard title={current.title} text={current.text} />
+     <ScenarioCard
+  title={current.title}
+  text={feedback === true ? current.text : null}
+/>
 
       <div
         style={{
