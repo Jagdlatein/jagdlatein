e6abb6dc6ec898d20bae2e7b78cf984a3ba367c4
Jagdlatein <hannes.ziegler@hotmail.com>
2026-09-02 19:24:07 +0200

    Update nachtjagd.js

--- pages/jagdpraxis/nachtjagd.js
+++ pages/jagdpraxis/nachtjagd.js
@@ -85,12 +85,12 @@ export default function Nachtjagd() {
 
     if (isCorrect) setScore((prev) => prev + 1);
 
-    setTimeout(() => {
-      setFeedback(null);
-      setLockButtons(false);
-      setStep((prev) => prev + 1);
-    }, 1200);
-  }
+ setTimeout(() => {
+  setFeedback(null);
+  setLockButtons(false);
+  setStep((prev) => prev + 1);
+}, isCorrect ? 3500 : 1200);
+}
 
   // ------------------------------------------------------------
   // ENDSEITE
@@ -143,7 +143,10 @@ export default function Nachtjagd() {
 
       <h1 style={{ fontSize: 34, marginBottom: 10 }}>Nachtjagd-Simulator</h1>
 
-      <ScenarioCard title={current.title} text={current.text} />
+      <ScenarioCard
+  title={current.title}
+  text={feedback === true ? current.text : null}
+/>
 
       <div
         style={{
